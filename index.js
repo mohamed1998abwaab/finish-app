6afad1064fd63d995c484faf89c3ddfe97135f8d
mahdi ali <127436608+mahdi-ali-flutter@users.noreply.github.com>
2024-06-11 11:35:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ mongo.connect(url1,{useNewUrlParser:true,useUnifiedTopology:true},
    const products=new mongo.model("products",product);
   
           ////upload iamge 
-/*
+
        const storage= multer.diskStorage({
             destination:function(req,file,cb){
               cb(null,path.join(__dirname,"./images"))
@@ -50,7 +50,7 @@ mongo.connect(url1,{useNewUrlParser:true,useUnifiedTopology:true},
           });
 
      const upload=multer({storage:storage});
-*/
+
 
    app.get('/data',(req,res,next)=>{
     fetchid=req.params.id  ;
